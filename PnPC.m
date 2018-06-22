b81ed93f3b0e3d41759dd35d18cb4634a0ddaa68
Andreas Vieregg <doctorwhom042@gmail.com>
2018-06-22 21:59:15 +0200

    slight modification of comment in .m file

--- PnPC.m
+++ PnPC.m
@@ -4,7 +4,7 @@ A = [0 .01 ; 0 -.001];
 B = [6.25 0 -1 ; 0 10 0];
 C = [1 0];
 
-% original controller using the pump as input.
+% original controller using the pump as output.
 
 s = tf('s');
 K0 = [0 ; (-.1*(s + .01)*(s+.001)^2)/(s^2*(s+.1)) ; 0];
